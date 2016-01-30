52634059f7b6ae01b677569e64cfd58080515aad
mathemortician1 <olcooksy@gmail.com>
2016-01-30 14:38:08 -0800

    Added delta, startTime to Timer Class
    yep.

--- src/Wrapper/Timer.java
+++ src/Wrapper/Timer.java
@@ -1,5 +1,29 @@
 package Wrapper;
 
 public class Timer {
-
+	
+	double deltaTime = 0, startTime ;
+	
+	public Timer () {
+		startTime = System.currentTimeMillis();	
+	}
+	
+	public double getTime(){
+		return System.currentTimeMillis() - startTime;
+	}
+	
+	public double getDeltaTime(){
+		return deltaTime;
+	}
+	
+	public void reset(){
+		startTime = System.currentTimeMillis();
+	}
+	
+	public boolean wait(double time){
+		if (time >= getTime())
+			return true;
+		return false;
+	}
+	
 }
