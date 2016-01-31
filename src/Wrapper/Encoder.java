58461dd33b625b0cf7d274cbd25e1d1379c55909
Jacob Golden-Needham <BUNN1E5@users.noreply.github.com>
2016-01-31 13:23:24 -0800

    Dammit Stephen

---
+++ src/Wrapper/Encoder.java
@@ -0,0 +1,18 @@
+package Wrapper;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+/**
+ * 
+ * @author Ghjf544912
+ *
+ */
+public class Encoder {
+	
+	
+	public double getAngle(double angle){
+		
+		return angle;
+	}
+	
+}
