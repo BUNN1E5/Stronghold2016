3db1f31f2c4d5be8015e9375b70fbdcba3c84459
Jacob Golden-Needham <BUNN1E5@users.noreply.github.com>
2016-01-31 13:25:30 -0800

    Turns out measurement is the same for treads as wheels

--- src/Measurements/Speed.java
+++ src/Measurements/Speed.java
@@ -7,24 +7,13 @@ package Measurements;
  */
 public class Speed {
 	
-	public double rawSpeed, treadLength;
-	
+	public double rawSpeed;
 	/**
 	 * 
 	 * @param rawSpeed The encoder's raw speed value.
 	 */
 	public Speed(double rawSpeed){
 		this.rawSpeed=rawSpeed;
-	}
-	
-	/**
-	 * 
-	 * @param rawSpeed The encoder's raw speed value.
-	 * @param treadLength The length of the treads on the row boat.
-	 */
-	public Speed(double rawSpeed, double treadLength){
-		this.rawSpeed=rawSpeed;
-		this.treadLength=treadLength;
 	}
 	
 	/**
@@ -77,15 +66,6 @@ public class Speed {
 	 * @return Speed in in/sec.
 	 */
 	public double getInPS(){
-		
-		return rawSpeed;
-	}
-	
-	/**
-	 * TODO: Add math to change raw speed to actual speed, remove rawSpeed as return variable.
-	 * @return Speed in terms of the number of tread cycles per minute.
-	 */
-	public double getTreadCyclesPM(){
 		
 		return rawSpeed;
 	}
