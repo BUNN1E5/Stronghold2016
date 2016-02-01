4cd0aca40c944ae2c257ba9e2a83c24b5d6ee8d9
Steve <stephen.barrack12@gmail.com>
2016-02-01 09:07:15 -0800

    All our constants
    ying

---
+++ src/Wrapper/Constants.java
@@ -0,0 +1,18 @@
+package Wrapper;
+
+/**
+ * 
+ * @author Ghjf544912 (By that I mean Barry)
+ *
+ */
+public class Constants {
+
+	static int[]	//Gotta have da 4matting!
+	DIO_PORT={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25},
+	RELAY_PORT={0,1,2,3},
+	ANALOG_PORT={0,1,2,3},
+	PWM_PORT={0,1,2,3,4,5,6,7,8,9},
+	PCM_PORT ={0,1,2,3,4,5,6,7},
+	CAN_ID= {0,1,2,3,4,5,6,7,8,9,10},
+	DRIVER_STATION_PORT={0,1,2,3,4,5};
+}
