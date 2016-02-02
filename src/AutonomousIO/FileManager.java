25e2bf0b8df53c681aece8f198b610f69c9a94c5
Sean McDonald <seanmm11323@gmail.com>
2016-02-01 20:36:36 -0800

    Started on AutonomousIO
    AutonomousIO is a system where autonomous can be recorded by simply
    controlling the robot, then played back later in autonomous mode

---
+++ src/AutonomousIO/FileManager.java
@@ -0,0 +1,17 @@
+package AutonomousIO;
+
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+
+public class FileManager {
+	
+	public FileManager(){
+		
+	}	
+	
+	public static String[] parseData(String data){
+		return data.split(",");
+	}
+	
+}
