d890e6e65912d86c8c6989da6344af77ae72513f
Sean McDonald <seanmm11323@gmail.com>
2016-02-02 19:52:20 -0800

    started controls manager

--- src/Wrapper/ControlsManager.java
+++ src/Wrapper/ControlsManager.java
@@ -1,5 +1,18 @@
 package Wrapper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.usfirst.frc.team4738.robot.Drive;
+
 public class ControlsManager {
 
+	HashMap<String, Gamepad> controllers = new HashMap<>();
+	
+	public ControlsManager(Gamepad... controllers){
+		
+	}
+	
+	
+	
 }
