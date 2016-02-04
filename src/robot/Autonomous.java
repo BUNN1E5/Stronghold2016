780aeb59502e36b9b417d8cb825800fdda1e67cc
Jacob Golden-Needham <BUNN1E5@users.noreply.github.com>
2016-02-04 00:03:54 -0800

    Update and rename Automous.java to Autonomous.java

--- src/robot/Autonomous.java
+++ src/robot/Autonomous.java
@@ -2,7 +2,7 @@ package robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
-public class Automous extends IterativeRobot{
+public class Autonomous extends IterativeRobot{
 		
 	public void autonomousInit(){
 		
