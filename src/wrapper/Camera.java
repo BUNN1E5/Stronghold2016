43e049eb9a5aee97250cad5a0be3602b761c2ccf
Jacob Golden-Needham <BUNN1E5@users.noreply.github.com>
2016-02-07 14:17:54 -0800

    Camera code now works

--- src/wrapper/Camera.java
+++ src/wrapper/Camera.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.CameraServer;
 /**
  * @author Jacob
  */
-public class Camera {
+public class Camera{
 	
 	int[] sessions;
 	Image frame;
@@ -22,31 +22,37 @@ public class Camera {
 	 * @param camNames Names of cameras I guess.
 	 */
 	public Camera(String... camNames){
+		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
 		sessions = new int[camNames.length];
 		rect = new NIVision.Rect(10, 10, 100, 100);
-		for(int i = 0; i > camNames.length; i++){
+		for(int i = 0; i < camNames.length; i++){
 			sessions[i] = NIVision.IMAQdxOpenCamera(camNames[i],  NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+			
 		}
+		NIVision.IMAQdxStartAcquisition(sessions[camIndex]);
+		NIVision.IMAQdxConfigureGrab(sessions[camIndex]);
 	}
 	
 	public void updateCapture(){
 		NIVision.IMAQdxGrab(sessions[camIndex], frame, 1);
-		NIVision.imaqDrawShapeOnImage(frame, frame, rect, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+		//NIVision.imaqDrawShapeOnImage(frame, frame, rect, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
 		CameraServer.getInstance().setImage(frame);
 	}
 	
 	public void changeCamera(int index){
-		NIVision.IMAQdxCloseCamera(sessions[camIndex]);
+		NIVision.IMAQdxStopAcquisition(sessions[camIndex]);
+		//NIVision.IMAQdxCloseCamera(sessions[camIndex]);
+
 		this.camIndex = index;
+		if(camIndex >= sessions.length){
+			camIndex = 0;
+		}
+		NIVision.IMAQdxConfigureGrab(sessions[camIndex]);
 		updateCapture();
 	}
 	
 	public void cycleCamera(){
-		changeCamera(camIndex++);
-		if(camIndex >= sessions.length){
-			camIndex = 0;
-		}
-		updateCapture();
+		changeCamera(camIndex+1);
 	}
 	
 }
