7bba5d34126452559efd875b46f08e18c87b6183
mathemortician1 <olcooksy@gmail.com>
2016-02-09 19:37:11 -0800

    Fixed Stephen's stupid math
    Seriously dude? It was just plus one and divided by 2. No logic needed.
    - Jacob

--- src/wrapper/CameraGimbal.java
+++ src/wrapper/CameraGimbal.java
@@ -14,44 +14,18 @@ public class CameraGimbal {
 	}
 	
 	public void setPan(double pan){
-		if(pan<0)
-			panServo.set(-1*(Math.abs(pan)+1)/2); 
-		else
-			panServo.set((pan+1)/2); 
+		panServo.set((pan + 1) / 2);
 	}
 	
 	public void setTilt(double tilt){
-		if(tilt<0)
-			tiltServo.set(-1*(Math.abs(tilt)+1)/2);
-		else
-			tiltServo.set((tilt+1)/2); 
+		tiltServo.set((tilt + 1) / 2);
 	}
 	
-	public void setPos(double pan, double tilt){
+	public void setPosition(double pan, double tilt){
 		this.setTilt(tilt);
 		this.setPan(pan);
 	}
 	
-	public void setDeltaPan(double pan){
-		if(pan>0){
-		panServo.set(panServo.get()+(pan+1)/2);
-		}else{
-			panServo.set(panServo.get()+(Math.abs(pan)+1)/2);
-		}
-	}
-	
-	public void setDeltaTilt(double tilt){
-		if(tilt>0){
-			tiltServo.set(tiltServo.get()+(tilt+1)/2);
-		}else{
-			tiltServo.set(tiltServo.get()-(Math.abs(tilt)+1)/2);
-		}	
-	}
-	
-	public void setDeltaPos(double pan, double tilt){
-		this.setDeltaPan(pan);
-		this.setDeltaTilt(tilt);
-	}
 	
 	public double getPan(){
 		return panServo.get();
