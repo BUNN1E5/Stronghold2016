5c58b5fe961f01879e0a74ab980b613a2fd8441f
Jacob Golden-Needham <therealbunn1e5@gmail.com>
2016-02-10 20:09:29 -0800

    Added parabolic arcade

--- src/robot/Teleop.java
+++ src/robot/Teleop.java
@@ -1,15 +1,25 @@
 package robot;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import wrapper.CameraGimbal;
 
 public class Teleop extends IterativeRobot{
 	
+	//AnalogGyro gyro;
+	CameraGimbal gimbal; 
+	
 	public void teleopInit(){
-		
+		//gyro = new AnalogGyro(0);
+		gimbal = new CameraGimbal(2, 3, 0);
 	}
 	
 	public void teleopPeriodic(){
-		
+		SmartDashboard.putDouble("Gyro", gimbal.gyro.getAngle());
+		SmartDashboard.putDouble("Gyro Normalized", (gimbal.gyro.getAngle() / 90));
+		gimbal.setTiltBasedOnGyro();
+		//gimbal.setTilt((gyro.getAngle() / 90));
 	}
 	
 }
--- src/wrapper/CameraGimbal.java
+++ src/wrapper/CameraGimbal.java
@@ -1,24 +1,39 @@
 package wrapper;
 
-import com.ni.vision.NIVision.RawData;
-
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class CameraGimbal {
 	
-	Servo panServo, tiltServo;
+	private Servo panServo, tiltServo;
+	public AnalogGyro gyro;
 	
 	public CameraGimbal(int panPort, int tiltPort){
 		panServo = new Servo(panPort);		
 		tiltServo = new Servo(tiltPort);
 	}
 	
+	/**
+	 * 
+	 * @param panPort
+	 * @param tiltPort
+	 * @param gyroPort GYRO IS IN THE ANALOG PORT!!!!
+	 */
+	public CameraGimbal(int panPort, int tiltPort, int gyroPort){
+		panServo = new Servo(panPort);		
+		tiltServo = new Servo(tiltPort);
+		gyro = new AnalogGyro(gyroPort);
+		
+	}
+	
+	
 	public void setPan(double pan){
 		panServo.set((pan + 1) / 2);
 	}
 	
 	public void setTilt(double tilt){
-		tiltServo.set((tilt + 1) / 2);
+		tiltServo.set(((tilt) + 1) / 2);
 	}
 	
 	public void setPosition(double pan, double tilt){
@@ -33,6 +48,18 @@ public class CameraGimbal {
 	
 	public double getTilt(){
 		return tiltServo.get();
+	}
+	
+	/**
+	 * Run this ONLY if you have a Gyro
+	 */
+	public void setTiltBasedOnGyro(){
+		try{
+			setTilt((gyro.getAngle() / 90));
+		} catch(NullPointerException e){
+			System.err.println("I FRICKEN SAID DON'T RUN THIS IF YOU DON'T HAVE A GYRO YOU IDIOT!!!!");
+		}
+		
 	}
 	
 }
