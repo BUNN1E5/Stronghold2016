bbaad83ee87c6af5711fa6485ca98511a4475935
mathemortician1 <olcooksy@gmail.com>
2016-02-15 15:19:32 -0800

    Revert "Was Ist Das?"
    This reverts commit d6efa90ff94c8b5ef2055298fe672e31894d846e.

--- src/robot/Test.java
+++ src/robot/Test.java
@@ -1,19 +1,15 @@
 package robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import wrapper.CameraGimbal;
-import wrapper.XboxController;
 
 public class Test extends IterativeRobot{
-		XboxController controller;
-		CameraGimbal gimble;
+		
 	public void testInit(){
-		controller = new XboxController(0);
-		gimble = new CameraGimbal(2, 3);
+		
 	}
 	
 	public void testPeriodic(){
-		gimble.setPosition(-controller.getRightStick().getX(), controller.getRightStick().getY());
+		
 	}
 	
 }
